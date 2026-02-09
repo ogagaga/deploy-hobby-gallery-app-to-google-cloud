ff59bdf99c0befc8b6b468d105adefaaad575c8b
y_ogagaga <170394+ogagaga@users.noreply.github.com>
2026-02-09 20:10:00 +0900

    fix: GCS からの画像読み込みを許可するために remotePatterns を追加

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
